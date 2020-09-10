2d4b6c361aad46b9b998059935e1d5a14e62ba12
João Paulo Lima <jptick@gmail.com>
2020-09-10 10:40:24 -0300

    Styling Landing Page Layout

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
-function App(){
+import Landing from './pages/Landing';
+
+import './assets/styles/global.css';
+
+function App() {
   return (
-    <div className="App">
-      <h1>Hello React</h1>
-    </div>
+    <Landing />
   );
 }
 
