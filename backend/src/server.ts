fd8cd7ca8aa73df8fc778cdf432cc397561ac117
João Paulo Lima <jptick@gmail.com>
2020-09-14 12:46:21 -0300

    Create post API route to Create Classes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,10 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (request, response)=>{
-  return response.json({message:"Hello Node"});
-})
+app.use(routes);
 
 app.listen(3333);
