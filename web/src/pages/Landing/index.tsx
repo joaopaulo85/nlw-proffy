81fcdd0fe71646f346d6b0c3484f79b62a9ec4ef
jp <jptick@gmail.com>
2020-10-15 16:44:53 -0300

    Update layout and connect with node API

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 import logoImg from '../../assets/images/logo.svg';
@@ -9,8 +9,18 @@ import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
 function Landing() {
+  const [totalConnections, setTotalConnections] = useState(0);
+
+  useEffect(() => {
+    api.get('connections').then(response => {
+      const { total } = response.data;
+
+      setTotalConnections(total);
+    });
+  }, []);
   return (
     <div id="page-landing">
       <div id="page-landing-content" className="container">
@@ -25,25 +35,24 @@ function Landing() {
         />
 
         <div className="buttons-container">
-          <Link to="/study" className="study" >
+          <Link to="/study" className="study">
             <img src={studyIcon} alt="Estudar" />
             Estudar
           </Link>
 
-          <Link to="/give-classes" className="give-classes" >
+          <Link to="/give-classes" className="give-classes">
             <img src={giveClassesIcon} alt="Estudar" />
             Dar aulas
           </Link>
         </div>
 
         <span className="total-connections">
-          Total de 200 conexões já realizadas
+          Total de {totalConnections} conexões já realizadas
           <img src={purpleHeartIcon} alt="Coração roxo" />
         </span>
-
       </div>
     </div>
-  )
+  );
 }
 
 export default Landing;
